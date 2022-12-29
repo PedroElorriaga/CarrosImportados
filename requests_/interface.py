7304af6c55919999e579590376ddb89f5c03f073
Pedro <pedro@MacBook-Pro-de-Pedro.local>
2022-12-29 12:46:08 -0300

    envio291222

---
+++ requests_/interface.py
@@ -0,0 +1,26 @@
+import tkinter
+import customtkinter
+from app import *
+
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("green")
+width = 450
+heigth = 500
+
+root = customtkinter.CTk()
+root.geometry(f"{width}x{heigth}+500+200")
+root.wm_resizable(False, False)
+root.title("Base de dados")
+
+frame = customtkinter.CTkFrame(root)
+frame.pack(pady=10, padx=50, fill="both", expand=True)
+customtkinter.CTkLabel(frame, text="Carros importados", font=("Roboto",20, "bold")).pack(pady=30)
+customtkinter.CTkLabel(frame, text="Qual consulta deseja realizar?", font=("Arial",14)).pack(pady=10)
+customtkinter.CTkButton(frame, text="Detalhes",font=("Roboto",14), height=30, command=exibir_detalhe).pack(pady=15)
+customtkinter.CTkButton(frame, text="Incluir",font=("Roboto",14), height=30).pack(pady=15)
+customtkinter.CTkButton(frame, text="Atualizar",font=("Roboto",14), height=30).pack(pady=15)
+customtkinter.CTkButton(frame, text="Deletar",font=("Roboto",14), height=30).pack(pady=15)
+
+customtkinter.CTkLabel(frame, text="Direitos reservados para PedroElorriaga ©", font=("Roboto", 8)).pack(pady=40)
+
+root.mainloop()
